46e780cead68c4d125c211238b5394ca1d3deff1
ReshetnikovIvan121 <ivanreshetnikov235@gmail.com>
2023-02-17 16:25:08 +0700

    lib myTerm.h

---
+++ src/libmyTerm/myTerm.h
@@ -0,0 +1,20 @@
+#pragma once
+
+enum Colors {
+  Black,
+  Red,
+  Green,
+  Yellow,
+  Blue,
+  Magenta,
+  Cyan,
+  White,
+  Unknown,
+  Default
+};
+
+int mt_clrscr();
+int mt_gotoXY(int x, int y);
+int mt_getscreensize(int *rows, int *cols);
+int mt_setbgcolor(enum Colors color);
+int mt_setbgcolor(enum Colors color);
