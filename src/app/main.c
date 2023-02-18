0e449e1ff62af13433c9fdb51f7d61406be3a8e0
ReshetnikovIvan121 <ivanreshetnikov235@gmail.com>
2023-02-18 12:18:02 +0700

    Edit main.c

--- src/app/main.c
+++ src/app/main.c
@@ -1,7 +1,18 @@
 #include <libcomputer/computerlib.h>
 #include <libmyTerm/myTerm.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(){
-    mt_gotoXY(5,5);
+    mt_clrscr();
+    mt_setfgcolor(Red);
+    mt_setbgcolor(White);
+    printf("HEllo");
+
+    int *r = malloc(sizeof(u_int));
+    int *c = malloc(sizeof(u_int));
+    mt_getscreensize(r,c);
+    mt_gotoXY(5,5); // 129, 129 Error
+    
     return 0;
 }
