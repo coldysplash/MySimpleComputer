d7a3d45b61780b553b6611678ee056088a29bf14
Решетников Иван Вадимович <iv121s24@mwp04.csc.local>
2023-02-20 08:45:05 +0700

    edit main.c

--- src/app/main.c
+++ src/app/main.c
@@ -5,14 +5,17 @@
 
 int main(){
     mt_clrscr();
+
+
+
+    #if 0
     mt_setfgcolor(Red);
     mt_setbgcolor(White);
     printf("HEllo");
-
     int *r = malloc(sizeof(u_int));
     int *c = malloc(sizeof(u_int));
     mt_getscreensize(r,c);
     mt_gotoXY(5,5); // 129, 129 Error
-    
+    #endif
     return 0;
 }
