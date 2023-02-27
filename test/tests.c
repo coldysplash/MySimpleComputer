f86213270e49e7eb10091940344e25eb6a2ef282
ReshetnikovIvan121 <ivanreshetnikov235@gmail.com>
2023-02-27 16:17:48 +0700

    Edit tests

--- test/tests.c
+++ test/tests.c
@@ -23,14 +23,14 @@ CTEST (test_memSave, sc_memorySave){
     sc_memoryInit();
     sc_memorySet(10, 555);
     sc_memorySet(15, 5);
-    sc_memorySave("testSave.bin");
-    sc_memorySave("testLoad.bin");
-    ASSERT_EQUAL(sc_memorySave("testSave.bin"), 0);
+    sc_memorySave("bin/testSave.bin");
+    sc_memorySave("bin/testLoad.bin");
+    ASSERT_EQUAL(sc_memorySave("bin/testSave.bin"), 0);
 }
 
 CTEST (test_memLoad, sc_memoryLoad){
     printf("\n");
     sc_memoryInit();
-    sc_memoryLoad("testLoad.bin");
-    ASSERT_EQUAL(sc_memoryLoad("testLoad.bin"), 0);
+    sc_memoryLoad("bin/testLoad.bin");
+    ASSERT_EQUAL(sc_memoryLoad("bin/testLoad.bin"), 0);
 }
