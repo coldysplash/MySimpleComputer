0bc18262a7c1df55494ee704a80ccecdd94c3488
ReshetnikovIvan121 <ivanreshetnikov235@gmail.com>
2023-03-19 18:59:46 +0700

    func setbigcharpos done

--- src/libmyBigChars/myBigChars.c
+++ src/libmyBigChars/myBigChars.c
@@ -1,10 +1,10 @@
 #include <fcntl.h>
+#include <libmyBigChars/myBigChars.h>
 #include <libmyTerm/myTerm.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <libmyBigChars/myBigChars.h>
 
 // acs chars
 #define CKBOARD ("a")  // ▒
@@ -64,55 +64,77 @@ bc_box (int x1, int y1, int x2, int y2)
   return 0;
 }
 
+int
+bc_printbigchar (int bigchar[], int x, int y, enum Colors frontcolor,
+                 enum Colors backcolor)
+{
 
-int bc_printbigchar (int bigchar[], int x, int y, enum Colors frontcolor, enum Colors backcolor) {
-
-  if (mt_setfgcolor(frontcolor) == -1 || mt_setbgcolor(backcolor) == -1 || mt_gotoXY(x, y) == -1){
-    return -1;
-  }
-
-  for(int i = 0; i < 4; i++){
-    int value = (bigchar[0] >> (i * 8)) & 0xff;
-    for(int j = 0; j < 8; j++){
-      mt_gotoXY(x + i, y + j);
-      int ch = (value >> j) & 0x00000001;
-      if(ch == 1){
-        bc_printA(CKBOARD);
-      }else{
-        write(1, " ", 2);
-      }
+  if (mt_setfgcolor (frontcolor) == -1 || mt_setbgcolor (backcolor) == -1
+      || mt_gotoXY (x, y) == -1)
+    {
+      return -1;
     }
-  }
 
-  for(int i = 0; i < 4; i++){
-    int value = (bigchar[1] >> (i * 8)) & 0xff;
-    for(int j = 0; j < 8; j++){
-      mt_gotoXY(x + 4 + i, y + j);
-      int ch = (value >> j) & 0x00000001;
-      if(ch == 1){
-        bc_printA(CKBOARD);
-      }else{
-        write(1, " ", 2);
-      }
+  for (int i = 0; i < 4; i++)
+    {
+      int value = (bigchar[0] >> (i * 8)) & 0xff;
+      for (int j = 0; j < 8; j++)
+        {
+          mt_gotoXY (x + i, y + j);
+          int ch = (value >> j) & 0x00000001;
+          if (ch == 1)
+            {
+              bc_printA (CKBOARD);
+            }
+          else
+            {
+              write (1, " ", 2);
+            }
+        }
     }
-  }
+
+  for (int i = 0; i < 4; i++)
+    {
+      int value = (bigchar[1] >> (i * 8)) & 0xff;
+      for (int j = 0; j < 8; j++)
+        {
+          mt_gotoXY (x + 4 + i, y + j);
+          int ch = (value >> j) & 0x00000001;
+          if (ch == 1)
+            {
+              bc_printA (CKBOARD);
+            }
+          else
+            {
+              write (1, " ", 2);
+            }
+        }
+    }
 
   return 0;
 }
 
-int bc_setbigcharpos (int big[], int x, int y, int value){
+int
+bc_setbigcharpos (int big[], int x, int y, int value)
+{
 
-  if(big == NULL || x < 0 || y < 0 || x > 7 || y > 7 || value > 1 || value < 0){
-    return -1;
-  }
+  if (big == NULL || x < 0 || y < 0 || x > 7 || y > 7 || value > 1
+      || value < 0)
+    {
+      return -1;
+    }
 
   int bc_position = x < 4 ? 0 : 1;
-  if(value == 1){
-      int val = (big[bc_position] >> (x * 8)) & 0xff;
-      printf("%X", val);
-  }else{
-    
-  }
+  x = x % 4;
+
+  if (value == 1)
+    {
+      big[bc_position] |= 1 << (x * 8 + y);
+    }
+  else
+    {
+      big[bc_position] &= ~(1 << (x * 8 + y));
+    }
 
   return 0;
 }
--- src/libmyBigChars/myBigChars.h
+++ src/libmyBigChars/myBigChars.h
@@ -3,5 +3,6 @@
 
 int bc_printA (char *str);
 int bc_box (int x1, int y1, int x2, int y2);
-int bc_printbigchar (int bigchar[], int x, int y, enum Colors frontcolor, enum Colors backcolor);
+int bc_printbigchar (int bigchar[], int x, int y, enum Colors frontcolor,
+                     enum Colors backcolor);
 int bc_setbigcharpos (int big[], int x, int y, int value);
