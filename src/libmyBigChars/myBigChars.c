09eab155b3743c3f22910bc9ff66c1f8b56c37c3
ReshetnikovIvan121 <ivanreshetnikov235@gmail.com>
2023-03-19 19:41:47 +0700

    formating

--- src/libmyBigChars/myBigChars.c
+++ src/libmyBigChars/myBigChars.c
@@ -139,46 +139,54 @@ bc_setbigcharpos (int big[], int x, int y, int value)
   return 0;
 }
 
-int bc_getbigcharpos(int big[], int x, int y, int *value){
-    if (big == NULL || value == NULL || x < 0 || y < 0 || x > 7 || y > 7)
+int
+bc_getbigcharpos (int big[], int x, int y, int *value)
+{
+  if (big == NULL || value == NULL || x < 0 || y < 0 || x > 7 || y > 7)
     {
       return -1;
     }
 
-    int bc_position = x < 4 ? 0 : 1;
-    x = x % 4;
+  int bc_position = x < 4 ? 0 : 1;
+  x = x % 4;
 
-    *value = big[bc_position] >> ((x * 8) + y) & 1;
+  *value = big[bc_position] >> ((x * 8) + y) & 1;
 
-    return 0;
-}
-
-int bc_bigcharwrite (int fd, int big[], int count){
-  if(big == NULL || count < 1){
-    return -1;
-  }
-  if (write(fd, big,(sizeof(int)) * count * 2) == -1)
-	{
-		return -1;
-	}
   return 0;
 }
 
-int bc_bigcharread (int fd, int big[], int need_count, int * count){
-  if(big == NULL || need_count < 1 || count == NULL){
-    *count = 0;
-    return -1;
-  }
+int
+bc_bigcharwrite (int fd, int big[], int count)
+{
+  if (big == NULL || count < 1)
+    {
+      return -1;
+    }
+  if (write (fd, big, (sizeof (int)) * count * 2) == -1)
+    {
+      return -1;
+    }
+  return 0;
+}
 
-  int cnt = read(fd, big, (sizeof(int)) * need_count * 2);
+int
+bc_bigcharread (int fd, int big[], int need_count, int *count)
+{
+  if (big == NULL || need_count < 1 || count == NULL)
+    {
+      *count = 0;
+      return -1;
+    }
+
+  int cnt = read (fd, big, (sizeof (int)) * need_count * 2);
 
   if (cnt == -1)
-	{
-    *count = 0;
-		return -1;
-	}
+    {
+      *count = 0;
+      return -1;
+    }
 
-  *count = cnt / (sizeof(int) * 2);
+  *count = cnt / (sizeof (int) * 2);
 
   return 0;
 }
