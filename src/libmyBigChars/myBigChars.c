fc12fb4d5b9f6af77671f2ebfccf409325afab65
ReshetnikovIvan121 <ivanreshetnikov235@gmail.com>
2023-03-19 19:41:20 +0700

    myBigCharslib done

--- src/libmyBigChars/myBigChars.c
+++ src/libmyBigChars/myBigChars.c
@@ -152,3 +152,33 @@ int bc_getbigcharpos(int big[], int x, int y, int *value){
 
     return 0;
 }
+
+int bc_bigcharwrite (int fd, int big[], int count){
+  if(big == NULL || count < 1){
+    return -1;
+  }
+  if (write(fd, big,(sizeof(int)) * count * 2) == -1)
+	{
+		return -1;
+	}
+  return 0;
+}
+
+int bc_bigcharread (int fd, int big[], int need_count, int * count){
+  if(big == NULL || need_count < 1 || count == NULL){
+    *count = 0;
+    return -1;
+  }
+
+  int cnt = read(fd, big, (sizeof(int)) * need_count * 2);
+
+  if (cnt == -1)
+	{
+    *count = 0;
+		return -1;
+	}
+
+  *count = cnt / (sizeof(int) * 2);
+
+  return 0;
+}
--- src/libmyBigChars/myBigChars.h
+++ src/libmyBigChars/myBigChars.h
@@ -7,3 +7,5 @@ int bc_printbigchar (int bigchar[], int x, int y, enum Colors frontcolor,
                      enum Colors backcolor);
 int bc_setbigcharpos (int big[], int x, int y, int value);
 int bc_getbigcharpos(int big[], int x, int y, int *value);
+int bc_bigcharwrite (int fd, int big[], int count);
+int bc_bigcharread (int fd, int big[], int need_count, int * count);
